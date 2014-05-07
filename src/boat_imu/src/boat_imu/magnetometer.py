be882e8b806e8f32a1e5ef958892ba97ab05688f
smaria <sms4g13@soton.ac.uk>
2014-05-07 12:44:22 +0000

    consider sign

--- src/boat_imu/src/boat_imu/magnetometer.py
+++ src/boat_imu/src/boat_imu/magnetometer.py
@@ -7,22 +7,27 @@ http://www.timzaman.com/?p=994
 
 
 from bbio import *
+def  transformReadable(raw_data):
+    mag = [0, 0, 0]
+    for i in range(3):
+        if (raw_data[2*i + 3] < 0x10):
+            mag[i] = raw_data[2*i + 3]*0x100 + raw_data[2*i + 1 + 3]
+        else:
+            mag[i] = -(0x10000 - (raw_data[2*i + 3]*0x100 + raw_data[2*i + 1 + 3])) 
+    # get the magnetometer measurements in x, y, z order
+    storage = mag[1]
+    mag[1] = mag[2]
+    mag[2] = storage
+    return mag
 
 Wire2.begin()
 while(1):
     Wire2.write(0x1e,2,1)
     # detailed magnetometer read, incl. mode, status
     raw_data = Wire2.read(0x1e, 0, 10)
-    raw_data = Wire2.read(0x1e, 3, 6)
     print(raw_data)
+    mag = transformReadable(raw_data)
+    print("x-value: %d" %mag[0]),
+    print("y-value: %d" %mag[1]),
+    print("z-value: %d" %mag[2])
 
-    mag_X = raw_data[0]*0x100 + raw_data[1]
-    mag_Z = raw_data[2]*0x100 + raw_data[3]
-    mag_Y = raw_data[4]*0x100 + raw_data[5]
-    print("x-value: %d" %mag_X),
-    print("y-value: %d" %mag_Y),
-    print("z-value: %d" %mag_Z)
-
-"""
-current calculation doesn't consider the sign of the MSB...
-"""

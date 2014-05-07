241ee63aacb900de2673e9a9dfcb472cb6d4e549
smaria <sms4g13@soton.ac.uk>
2014-05-07 15:49:26 +0000

    choose servo values that are a bit safer
    choosing the wrong angles might break things, therefore these servo
    values were choosen considering the sail and rudder servo

--- src/boat_servo_sail/src/boat_servo_sail/servo_control.py
+++ src/boat_servo_sail/src/boat_servo_sail/servo_control.py
@@ -9,8 +9,8 @@ import time
 
 servo1 = Servo(PWM1A)
 servo2 = Servo(PWM2A)
-servo1.write(20)
-servo2.write(20)
+servo1.write(90)
+servo2.write(90)
 time.sleep(2)
-servo1.write(0)
-servo2.write(0)
+servo1.detach()
+servo2.detach()

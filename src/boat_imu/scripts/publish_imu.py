5a5c115476b5d05de075132ae399c8d2bdd156b9
anonym <no@adress.net>
2014-06-02 14:15:06 +0200

    publish measurement data with header

--- src/boat_imu/scripts/publish_imu.py
+++ src/boat_imu/scripts/publish_imu.py
@@ -3,22 +3,34 @@
 
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import Header
 from boat_imu.msg import measurements
 
 def talker():
     pubString = rospy.Publisher('chatter', String, queue_size=10)
     pubAcc = rospy.Publisher('Accelerometer', measurements, queue_size=10)
+
     rospy.init_node('talker', anonymous=True)
     r = rospy.Rate(10) # 10hz
+    h = Header()
     while not rospy.is_shutdown():
+        # generate header
+        #h = Header()
+        h.stamp = rospy.Time.now()
+ 
         str = "hello world %s"%rospy.get_time()
+
         #rospy.loginfo(str)
         pubString.publish(str)
         int = 1
         #rospy.loginfo(int)
         pubString.publish(int)
-        test_measurement = measurements(1,2,3)
-        #rospy.loginfo(test_measurement)
+        test_measurement = measurements(h,1,2,3)
+        rospy.loginfo(test_measurement)
+        # in the loginfo the seq number stays 0
+        # the actual messages contain increasing numbers
+        # as set by the publisher
+
         pubAcc.publish(test_measurement)
 
         r.sleep()

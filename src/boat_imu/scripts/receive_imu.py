8151aa7630d20794ac3988510cd11509c1bd3f57
anonym <no@adress.net>
2014-06-05 17:28:15 +0200

    add generic storage function for header+3d ROS msg

--- src/boat_imu/scripts/receive_imu.py
+++ src/boat_imu/scripts/receive_imu.py
@@ -15,7 +15,7 @@ history_length = 5
  AirPressure[0] newest """
 airPressure = [0 for x in range(history_length)]
 air = [0 for x in range(history_length)]
-
+accelerometer = [["header", 0, 0, 0] for x in range(history_length)]
 
 def accelerometerPrint(data):
     rospy.loginfo(rospy.get_caller_id()+"""
@@ -35,13 +35,20 @@ def storeAirPressure(data):
     print(airPressure)
 
 def storeIntROS(data, storage):
-""" insert new Int value received from ROS in
+    """ insert new Int value received from ROS in
     the storage list, remove oldest value
     """
     storage.insert(0,data.data)
     storage.pop()
     print(storage)
 
+def store3dROS(data, storage):
+    """ insert new header + three directional measurements
+    in the handed over storage list, remove oldest value
+    """
+    storage.insert(0,[data.header, data.x_value, data.y_value, data.z_value])
+    storage.pop()
+    print(storage)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
@@ -49,6 +56,7 @@ def listener():
     #rospy.Subscriber("Accelerometer", measurements, accelerometerPrint)
     rospy.Subscriber("AirPressure", Int64, storeAirPressure)
     rospy.Subscriber("AirPressure", Int64, storeIntROS, air)
+    rospy.Subscriber("Accelerometer", measurements, store3dROS, accelerometer)
     rospy.spin()
                                                     
 

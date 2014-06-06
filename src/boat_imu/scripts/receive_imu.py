12296680cf45df7081b89fdeaf5f8d6a053099c4
anonym <no@adress.net>
2014-06-07 01:24:59 +0200

    add some logging - careful, two different ways!
    careful with debugging:
    as soon as a rosnode is started,
    the ros logging needs to be used.
    It doesn't work before, and apparently the python logging
    doesn't work for ros

--- src/boat_imu/scripts/receive_imu.py
+++ src/boat_imu/scripts/receive_imu.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # this line is needed so the execution happens in python environment
 
-
-
 import rospy
 from std_msgs.msg import Int64
 from std_msgs.msg import Header
 from boat_imu.msg import measurements
-
 
 # number of remembered values
 history_length = 5
@@ -19,7 +16,9 @@ magnetometer = [["header", 0, 0, 0] for x in range(history_length)]
 gyroscope = [["header", 0, 0, 0] for x in range(history_length)]
 # TODO make this a dictionary, that contains: name, header, values
 # maybe even dimension?
-
+print('test')
+#rospy.loginfo("some text")
+print('test')
 
 def storeIntROS(data, storage):
     """ insert new Int value received from ROS in
@@ -27,7 +26,8 @@ def storeIntROS(data, storage):
     """
     storage.insert(0,data.data)
     storage.pop()
-    print("add value" + str(storage[0]))
+    rospy.logdebug("add value" + str(storage[0]))
+    rospy.logdebug(storage)
 
 def store3dROS(data, storage):
     """ insert new header + three directional measurements
@@ -35,10 +35,13 @@ def store3dROS(data, storage):
     """
     storage.insert(0,[data.header, data.x_value, data.y_value, data.z_value])
     storage.pop()
-    print("add value" + str(storage[0]))
+    rospy.logdebug("add value" + str(storage[0]))
+    rospy.logdebug(storage)
 
-def listener():
+
+def imu_listener():
     rospy.init_node('listener', anonymous=True)
+    rospy.loginfo('ros imu listener started')
     rospy.Subscriber("AirPressure", Int64, storeIntROS, airPressure)
     rospy.Subscriber("Accelerometer", measurements, store3dROS, accelerometer)
     rospy.Subscriber("Magnetometer", measurements, store3dROS, magnetometer)
@@ -47,4 +50,5 @@ def listener():
                                                     
 
 if __name__ == '__main__':
-    listener()
+    imu_listener()
+

f93b10589133e9dce23fd2cce7bde57431d94143
smaria <sms4g13@soton.ac.uk>
2014-06-15 15:28:46 +0200

    add pid control package

---
+++ src/boat_pid_control/scripts/pid_control.py
@@ -0,0 +1,5 @@
+import rospy
+from boat_imu.msg import measurements
+from boat_imu.scripts import receive_imu
+import boat_imu.scripts.receive_imu
+#from ..batoat_imu.scripts.receive_imu import accelerometer

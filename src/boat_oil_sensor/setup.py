e4f43c72114c7d755ffeb0162dff0536047ae151
smaria <sms4g13@soton.ac.uk>
2014-08-14 14:58:08 +0200

    add oil sensor package

---
+++ src/boat_oil_sensor/setup.py
@@ -0,0 +1,11 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+## from: docs.ros.org/api/catkin/html/howto/installing_python.html
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['boat_oil_sensor'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
---
+++ src/boat_oil_sensor/src/boat_oil_sensor/test.py
@@ -0,0 +1,2 @@
+def test():
+    print("oil sensor functions work")

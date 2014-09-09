da0a215858dc3b4a04e6af40d03127595a0b7333
smaria <sms4g13@soton.ac.uk>
2014-09-10 00:39:17 +0200

    add function structure (untested)

--- src/boat_pid_control/src/boat_pid_control/rudderPID.py
+++ src/boat_pid_control/src/boat_pid_control/rudderPID.py
@@ -25,17 +25,19 @@ DERIVATIVE_GAIN = 0
 currentHeading = 23
 goalHeading = 35
 
-# with new ROS input for goal or current heading
+def get_pid(currentHeading, goalHeading):
+    # with new ROS input for goal or current heading
 
-# Error calculation for angular error!
-error = currentHeading - goalHeading
+    # Error calculation for angular error!
+    error = currentHeading - goalHeading
 
 
-p = error * PROPORTIONAL_GAIN
-i = 0
-d = 0
+    p = error * PROPORTIONAL_GAIN
+    i = 0
+    d = 0
 
-correction = p + i + d
+    correction = p + i + d
+    rudder_position = 2
+    #translate correction to servo change ...
 
-#translate correction to servo change ...
-
+return rudder_position

ef3710cbc9f24e9b8f5cb37554d0cf2fae51e261
smaria <sms4g13@soton.ac.uk>
2014-09-09 18:10:38 +0200

    fix wrong names for publishing, remove printlines

--- src/boat_curr_heading/src/boat_curr_heading/goalHeading.py
+++ src/boat_curr_heading/src/boat_curr_heading/goalHeading.py
@@ -2,14 +2,8 @@ from sensor_msgs.msg import NavSatFix
 from LatLon import *
 
 def calc_heading(position, waypoint):
-    print position.latitude
-    print position.longitude
-    angle = 23
-    print angle
-
     pos = LatLon(Latitude(position.latitude), Longitude(position.longitude))
     wp = LatLon(Latitude(waypoint[0]), Longitude(waypoint[1]))
     angle = pos.heading_initial(wp)
-    print(angle)
     return angle
 

6d45538554cf6feca838706a535e7e43c7c66983
smaria <sms4g13@soton.ac.uk>
2014-09-09 19:21:14 +0200

    full set of functions for looking up angles
    boat, wind angle can now be used to calculate appropriate sail angle

--- src/boat_servo_sail/src/boat_servo_sail/sail_angle.py
+++ src/boat_servo_sail/src/boat_servo_sail/sail_angle.py
@@ -1,5 +1,11 @@
 import bisect
 
+def calculate_angle_boat_wind(boat_compass, wind_direction):
+    angle = abs(boat_compass - wind_direction)
+    if angle > 180:
+        angle = 360 - angle
+    return angle
+
 
 def assign_length(diff_wind_boat):
     angles = [
@@ -19,8 +25,15 @@ def assign_length(diff_wind_boat):
         ]
 
     angles.sort()
-    position = bisect.bisect_right(angles, diff_wind_boat)
+    position = bisect.bisect_right(angles, (diff_wind_boat,))
     sail_length = angles[position][1]
     return sail_length
 
-assign_length(42)
+def find_length(boat_compass, wind_direction):
+    angle_boat_wind = calculate_angle_boat_wind(boat_compass, wind_direction)
+    sail_length = assign_length(angle_boat_wind)
+    return sail_length
+
+
+#print(find_length(87, 180))
+

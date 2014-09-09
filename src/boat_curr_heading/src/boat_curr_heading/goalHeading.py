48bc3a5c2a0bd54e6d1cdd90431e0c0cb186b5f6
smaria <sms4g13@soton.ac.uk>
2014-09-09 17:48:25 +0200

    add heading calculation

--- src/boat_curr_heading/src/boat_curr_heading/goalHeading.py
+++ src/boat_curr_heading/src/boat_curr_heading/goalHeading.py
@@ -1,6 +1,15 @@
-def calc_heading(position):
+from sensor_msgs.msg import NavSatFix
+from LatLon import *
+
+def calc_heading(position, waypoint):
+    print position.latitude
+    print position.longitude
     angle = 23
+    print angle
 
+    pos = LatLon(Latitude(position.latitude), Longitude(position.longitude))
+    wp = LatLon(Latitude(waypoint[0]), Longitude(waypoint[1]))
+    angle = pos.heading_initial(wp)
+    print(angle)
     return angle
-
 

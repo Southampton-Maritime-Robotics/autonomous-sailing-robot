d7487c67926ff83b7bee4744826ac88ae715bc38
smaria <sms4g13@soton.ac.uk>
2014-09-10 01:37:59 +0200

    correct angle difference calculation

--- src/boat_pid_control/src/boat_pid_control/rudderPID.py
+++ src/boat_pid_control/src/boat_pid_control/rudderPID.py
@@ -18,30 +18,36 @@ import rospy
 from pid_controller_class import PID
 
 PROPORTIONAL_GAIN = 0.1
-INTEGRAL_GAIN = 0
+INTEGRAL_GAIN = 0.05
 DERIVATIVE_GAIN = 0
 INTEGRAL_LIMIT = 1
 
-controller = PID(PROPORTIONAL_GAIN, INTEGRAL_GAIN, DERIVATIVE_GAIN, INTEGRAL_LIMIT, -INTEGRAL_LIMIT)
+controller = PID(PROPORTIONAL_GAIN, INTEGRAL_GAIN, DERIVATIVE_GAIN, 
+                INTEGRAL_LIMIT, -INTEGRAL_LIMIT)
 
 currentHeading = 23
 goalHeading = 35
 
 def angle_difference(currentHeading, goalHeading):
     difference = goalHeading - currentHeading
-    if difference < 0:
+    if difference < -180:
         difference += 360
-    elif difference > 360:
+    elif difference > 180:
         difference -= 360
     return difference
 
 def get_pid(currentHeading, goalHeading):
     # with new ROS input for goal or current heading
-
-    # TODO Error calculation for angular error!
     error = angle_difference(currentHeading,goalHeading)
     correction = controller.update_PID(error)
     rudder_position = 2
-    #translate correction to servo change ...
 
-    return rudder_position
+    # clamp correction to max, min rudder positions
+    if correction < -1:
+        correction = -1
+    elif correction > 1:
+        correction = 1
+    return correction
+
+print(get_pid(18, 22))
+
